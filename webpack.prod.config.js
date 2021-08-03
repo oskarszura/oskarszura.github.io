b7ac09ffd142b01f06891e813149d847f8786d48
Oskar Szura <oskar.szura@gmail.com>
2021-08-03 19:18:15 +0200

    CODA-201 - Refactor codebase (#3)

---
+++ webpack.prod.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+    resolve: {
+        modules: [
+            path.resolve(__dirname, './node_modules'),
+            path.resolve(__dirname, '.'),
+        ],
+    },
+    context: `${__dirname}/client`,
+    output: {
+        path: `${__dirname}/public`,
+        filename: '[name].js',
+    },
+    entry: {
+        scripts: ['regenerator-runtime/runtime', './app.jsx'],
+        css: './app.scss',
+    },
+    module: {
+        rules: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            resolve: {
+                extensions: ['.js', '.jsx'],
+            },
+            use: {
+                loader: 'babel-loader',
+            },
+        }, {
+            test: /\.scss$/,
+            use: [
+                {
+                    loader: MiniCssExtractPlugin.loader,
+                },
+                'css-loader',
+                'sass-loader',
+            ],
+        }, {
+            test: /\.(png|jpg|woff|woff2|eot|ttf|svg)$/,
+            use: 'url-loader',
+        }],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'styles.css',
+        }, { allChunks: true }),
+    ],
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true,
+            }),
+        ],
+    },
+};
